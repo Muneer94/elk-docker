bbef88bb6ac45e1474c496d99dec91ac1f8abc1e
Muneer <sdmuneer89@gmail.com>
2021-08-19 03:25:33 +0530

    Check Docker

---
+++ tests/test_es.py
@@ -0,0 +1,48 @@
+import pytest
+from . import utils
+ 
+from scrapi.linter.document import NormalizedDocument, RawDocument
+from scrapi.processing.elasticsearch import ElasticsearchProcessor
+ 
+test_db = ElasticsearchProcessor()
+es = test_db.manager.es
+
+RAW = RawDocument(utils.RAW_DOC)
+NORMALIZED = NormalizedDocument(utils.RECORD)
+ 
+ 
+@pytest.mark.elasticsearch
+def test_process_normalized():
+    test_db.process_normalized(RAW, NORMALIZED, index='test')
+ 
+    results = es.search(index='test', doc_type=RAW['source'])
+    assert (len(results['hits']['hits']) == 1)
+ 
+ 
+@pytest.mark.elasticsearch
+def test_versions():
+    NORMALIZED['source'] = RAW['source']
+    NORMALIZED['_id'] = RAW['docID']
+    test_db.process_normalized(RAW, NORMALIZED, index='test')
+ 
+    old_title = NORMALIZED['title']
+    result = es.search(index='test', doc_type=RAW['source'])['hits']['hits'][0]
+ 
+    assert (result['_source']['title'] == old_title)
+ 
+    NORMALIZED['title'] = 'a new title'
+    test_db.process_normalized(RAW, NORMALIZED, index='test')
+    results = es.search(index='test', doc_type=RAW['source'])
+    assert (len(results['hits']['hits']) == 1)
+    assert (results['hits']['hits'][0]['_source']['title'] == 'a new title')
+ 
+ 
+@pytest.mark.elasticsearch
+def test_deleted_document():
+    NORMALIZED['source'] = RAW['source']
+    NORMALIZED['_id'] = RAW['docID']
+    NORMALIZED['shareProperties']['status'] = 'deleted'
+    test_db.process_normalized(RAW, NORMALIZED, index='test')
+ 
+    results = es.search(index='test', doc_type=RAW['source'])
+    assert (len(results['hits']['hits']) == 0)
