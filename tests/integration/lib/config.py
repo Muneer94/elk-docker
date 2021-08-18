6a3589bfe540799b37b897957a5220bd8cb29573
Muneer <sdmuneer89@gmail.com>
2021-08-19 01:58:38 +0530

    Check Docker

--- tests/integration/lib/config.py
+++ tests/integration/lib/config.py
@@ -53,3 +53,12 @@ print(kibana)
 browser.type = os.getenv('BROWSER', Chrome)
 browser.headless = os.getenv('RUN_HEADLESS_BROWSER', True)
 settings.driver_class = check_browser(browser.type)
+
+
+from selenium import webdriver
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--window-size=1420,1080')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(chrome_options=chrome_options)
